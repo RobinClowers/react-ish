e58b26511a9919de81e8e77bf789a7a5a682bf6b
Robin Clowers <robin.clowers@gmail.com>
2019-11-18 13:39:10 -0800

    Render with innerHtml

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,29 @@
-document.write('<h1>Hello World</h1>')
+interface DomNode {
+  tag: string;
+  children: DomNode[] | string | null;
+}
+
+const renderComponent = (): DomNode => {
+  return {
+    tag: 'div',
+    children: 'hello world',
+  }
+}
+
+const renderChildren = (node: DomNode) => {
+  if (node.children instanceof Array) {
+    node.children.map(n => render(n))
+  }
+  return node.children
+}
+
+const render = (node: DomNode): string => {
+  return `<${node.tag}>${renderChildren(node)}</${node.tag}>`
+}
+
+const rendered = render(renderComponent())
+console.log(rendered)
+const element = document.getElementById('app')
+if (element) {
+  element.innerHTML = rendered
+}
