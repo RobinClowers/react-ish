0985b5742fbd4ed3ad3569332cc4c797e10082f9
Robin Clowers <robin.clowers@gmail.com>
2019-11-18 15:37:54 -0800

    Move more code into dom render function

--- src/dom.ts
+++ src/dom.ts
@@ -10,14 +10,18 @@ export interface Attribute {
   [key: string]: string;
 }
 
-export const render = (node: DomNode): Element => {
+export const render = (node: DomNode, target: HTMLElement): void => {
+  target.appendChild(renderImpl(node))
+}
+
+const renderImpl = (node: DomNode): Element => {
   const el = document.createElement(node.tag)
   if (node.props.children instanceof Array) {
     for (const n of node.props.children) {
       if (n.tag === 'TEXT') {
         el.appendChild(document.createTextNode(n.props.value))
       } else {
-        el.appendChild(render(n))
+        el.appendChild(renderImpl(n))
       }
     }
   }
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,5 @@
 import { render, DomNode } from './dom'
 import App from './App'
 
-const rendered = render(App())
 const element = document.getElementById('app')
-if (element) {
-  element.appendChild(rendered)
-}
+element && render(App(), element)
